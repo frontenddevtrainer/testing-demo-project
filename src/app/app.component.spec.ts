0a075fd369a5d97e250ac68b7b683d7ac028c727
Admin <admin@example.com>
2023-10-30 10:02:53 +0000

    Added check for heading text.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent, HeaderComponent]
   }));
 
   it('should create the app', () => {
@@ -14,16 +15,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testing-demo-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-demo-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-demo-project app is running!');
-  });
+  
 });
--- src/app/component/header/header.component.spec.ts
+++ src/app/component/header/header.component.spec.ts
@@ -8,7 +8,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [HeaderComponent],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -18,4 +18,20 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should show the default header value", ()=>{
+      const heading = (fixture.nativeElement as HTMLElement).querySelector('h1');
+      expect(heading).toBeTruthy();
+      expect(heading?.textContent).toBe("Application Header");
+  })
+
+  it('should have heading as "Music Store" in H1', ()=>{
+      component.heading = "Music Store";
+      fixture.detectChanges();
+      const heading = (fixture.nativeElement as HTMLElement).querySelector('h1');
+      expect(heading).toBeTruthy();
+      expect(heading?.textContent).toBe("Music Store");
+  })
+
+
 });
